7f87661b56c5fa80594f09b2d1bcf49ba5d1a818
MangoDowner <353988117@qq.com>
2018-07-26 15:23:43 +0800

    vendor:sm4取代aes

--- vendor/github.com/hyperledger/fabric/bccsp/gm/keyderiv.go
+++ vendor/github.com/hyperledger/fabric/bccsp/gm/keyderiv.go
@@ -18,19 +18,17 @@ import (
 	"errors"
 
 	"fmt"
-	"math/big"
-
 	"github.com/hyperledger/fabric/bccsp"
-	"github.com/tjfoc/gmsm/sm2"
+	"crypto/hmac"
 	"reflect"
 )
 
-type gmsm2PrivateKeyKeyDeriver struct {
+type gmsm4PrivateKeyKeyDeriver struct {
 	bccsp *impl
 }
 
 // sm2私钥派生
-func (kd *gmsm2PrivateKeyKeyDeriver) KeyDeriv(k bccsp.Key, opts bccsp.KeyDerivOpts) (dk bccsp.Key, err error) {
+func (kd *gmsm4PrivateKeyKeyDeriver) KeyDeriv(k bccsp.Key, opts bccsp.KeyDerivOpts) (dk bccsp.Key, err error) {
 	// Validate opts
 	if opts == nil {
 		return nil, errors.New("Invalid opts parameter. It must not be nil.")
@@ -39,41 +37,11 @@ func (kd *gmsm2PrivateKeyKeyDeriver) KeyDeriv(k bccsp.Key, opts bccsp.KeyDerivOp
 	aesK := k.(*gmsm4PrivateKey)
 	fmt.Println(reflect.TypeOf(opts)) //*bccsp.HMACDeriveKeyOpts
 	switch opts.(type) {
-	// Re-randomized an ECDSA private key
-	case *bccsp.GMSM2ReRandKeyOpts:
-		reRandOpts := opts.(*bccsp.GMSM2ReRandKeyOpts)
-		tempSK := &sm2.PrivateKey{
-			PublicKey: sm2.PublicKey{
-				Curve: gmsm2K.privKey.Curve,
-				X:     new(big.Int),
-				Y:     new(big.Int),
-			},
-			D: new(big.Int),
-		}
-
-		var k = new(big.Int).SetBytes(reRandOpts.ExpansionValue())
-		var one = new(big.Int).SetInt64(1)
-		n := new(big.Int).Sub(gmsm2K.privKey.Params().N, one)
-		k.Mod(k, n)
-		k.Add(k, one)
-
-		tempSK.D.Add(gmsm2K.privKey.D, k)
-		tempSK.D.Mod(tempSK.D, gmsm2K.privKey.PublicKey.Params().N)
-
-		// Compute temporary public key
-		tempX, tempY := gmsm2K.privKey.PublicKey.ScalarBaseMult(k.Bytes())
-		tempSK.PublicKey.X, tempSK.PublicKey.Y =
-			tempSK.PublicKey.Add(
-				gmsm2K.privKey.PublicKey.X, gmsm2K.privKey.PublicKey.Y,
-				tempX, tempY,
-			)
-
-		// Verify temporary public key is a valid point on the reference curve
-		isOn := tempSK.Curve.IsOnCurve(tempSK.PublicKey.X, tempSK.PublicKey.Y)
-		if !isOn {
-			return nil, errors.New("Failed temporary public key IsOnCurve check.")
-		}
-		return &gmsm2PrivateKey{tempSK}, nil
+	case *bccsp.HMACDeriveKeyOpts:
+		hmacOpts := opts.(*bccsp.HMACDeriveKeyOpts)
+		mac := hmac.New(kd.bccsp.conf.hashFunction, aesK.privKey)
+		mac.Write(hmacOpts.Argument())
+		return &gmsm4PrivateKey{mac.Sum(nil), true}, nil
 	default:
 		return nil, fmt.Errorf("Unsupported 'KeyDerivOpts' provided [%v]", opts)
 	}
